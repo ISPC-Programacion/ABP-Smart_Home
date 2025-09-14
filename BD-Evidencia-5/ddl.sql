876b79731f05c3c699f29d4d7dc61ffe61da9d92
BrunoRazzotti <brunorazzotti99@gmail.com>
2025-09-14 18:34:21 -0300

    Comando creación DB

---
+++ BD-Evidencia-5/ddl.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Usuario (
+  id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  correo VARCHAR(50) UNIQUE NOT NULL,
+  contraseña VARCHAR(50) NOT NULL,
+  telefono VARCHAR(20)
+);
+
+CREATE TABLE TipoDispositivo (
+  id_tipo_dispositivo INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Dispositivo (
+  id_dispositivo INT AUTO_INCREMENT PRIMARY KEY,
+  esencial BOOLEAN,
+  configuracion VARCHAR(100),
+  estado VARCHAR(20),
+  id_tipo_dispositivo INT,
+  id_usuario INT,
+  FOREIGN KEY (id_tipo_dispositivo) REFERENCES TipoDispositivo(id_tipo_dispositivo),
+  FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+);
+
+CREATE TABLE TipoEvento (
+  id_tipo_evento INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Evento (
+  id_evento INT AUTO_INCREMENT PRIMARY KEY,
+  id_dispositivo INT,
+  fecha_generacion DATETIME,
+  id_tipo_evento INT,
+  FOREIGN KEY (id_dispositivo) REFERENCES Dispositivo(id_dispositivo),
+  FOREIGN KEY (id_tipo_evento) REFERENCES TipoEvento(id_tipo_evento)
+);
+
